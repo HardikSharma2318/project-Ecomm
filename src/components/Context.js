79b8f5f3f1384f92e34116a6e3b24c01b9fdd773
HardikSharma2318 <36570458+HardikSharma2318@users.noreply.github.com>
2019-09-12 17:26:36 +0530

    Update Context.js

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,40 +1,96 @@
 import React, { Component } from 'react'
 import {storeProducts,detailProduct} from './Data';
+import {storeProducts1,detailProduct1} from './Data';
+import {storeProducts2,detailProduct2} from './Data';
+import {storeProducts3,detailProduct3} from './Data';
 
-
-const ProductContext = React.createContext(); 
+const ProductContext = React.createContext(); //default values can be there we wish to create
+// to create both provider ans consumer
 
 //Place the ProductContext.Provider on top of the entire application.i.e in index.js
 //Do the same thing as done in ProductList i.e getting data from the Data.js and put it in an array.
 class ProductProvider extends Component {
     state={
-        products: [],  //assigning the objects to the variables, means passing them as a reference, storeProducts is the array of objects
+        products: [],
+       /* products_c1:[],
+        products_c2:[],
+        products_c3:[], */ //assigning the objects to the variables, means passing them as a reference, storeProducts is the array of objects
         detailProduct:detailProduct
     };
     componentDidMount(){
         this.setProducts();
+       // this.setFashion();
     }
-    setProducts = () => {
-        let tempProducts = [];
-        storeProducts.forEach( item => {
-        const singleItem = {...item};
-        tempProducts = [...tempProducts, singleItem];
-        });
+    setProducts = (searchInputVal) => { debugger
+        //let tempProducts = [...storeProducts];
+        this.getData("mobile");
+        let smidvar = this.midvar;
+        let filterData=[];
+        
+        //
+      // console.log(this.state.products);
+        if(searchInputVal) {
+            filterData = smidvar.productdetail.filter((item)=>item.title ===  searchInputVal)
+            this.setState({
+                products: filterData
+            })
+        }else{
         this.setState( () => {
-            return {products: tempProducts};
+            return {products: smidvar.productdetail};
         });
+        }
     };
+    getData = (cagname) => {
+        let trmppr = [...storeProducts];
+        this.originaldata = trmppr;
+
+
+       this.midvar = this.originaldata.find ((item)=>{
+            if(item.category === cagname){
+              return item.productdetail;
+            }
+        });
+        this.setData(this.midvar.productdetail);
+      
+    };
+    setData = (cagdata) => {
+        
+       this.setState({products:cagdata});
+
+   };
+    //setFashion = () => {
+    //    let tempProducts = [];
+    //    storeProducts1.forEach( item => {
+    //    const singleItem = {...item};
+    //    tempProducts = [...tempProducts, singleItem];
+    //    });
+    //    //
+    //   
+    //    this.setState( () => {
+    //        return {products_c1: tempProducts};
+    //    });
+    //};
+
+
     getItem = id => {
         const product = this.state.products.find(item =>{return item.id === id});
         return product;
     };
+    //getfashionItem = id => {
+    //const fashionprod = this.state.products_c1.find(item =>{return item.id === id});
+    //    return fashionprod;
+    //}
+    //handlefashionDetail = (id) =>{
+    //    let fashionprod = this.getfashionItem(id);
+    //    this.setState(() => {return {detailProduct: fashionprod}})
+    //};
     handleDetail = (id) =>{ // To open up the details page
      let product = this.getItem(id);
-     this.setState( () => {return {detailProduct: product}
-    
-    })          
+     this.setState({detailProduct: product})  
+     console.log(detailProduct);        
     };
     addToCart = (id) =>{ // To add the item to the cart
+        console.log("ur product id is",{id});
         //let tempProducts = [...this.state.products];
 
 
@@ -46,7 +102,7 @@ class ProductProvider extends Component {
     render() {
         return (
            // Provider is returned 
-            <ProductContext.Provider value={{...this.state,handleDetail:this.handleDetail,addToCart:this.addToCart}}>
+            <ProductContext.Provider value={{...this.state,handleDetail:this.handleDetail,addToCart:this.addToCart,setProducts:this.setProducts,getData:this.getData}}>
                 {this.props.children}    {/* returning all the children (within this component) in our application */}
             </ProductContext.Provider>
         );
